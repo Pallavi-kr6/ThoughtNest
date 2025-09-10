8d2f1b4ba00c6db1b072425c3b021cb402e5a12b
pallavi <pallavi6315@gmail.com>
2025-09-10 21:23:44 +0530

    Add start script for Render deployment

--- app.js
+++ app.js
@@ -41,7 +41,8 @@ app.get("/", (req, res) => {
 });
 
 
-const PORT = 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
